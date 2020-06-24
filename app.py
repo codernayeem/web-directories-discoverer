d9361a88dbce046ac601e5756a090f04a372de5a
Md Nayeem <codernayeem898@gmail.com>
2020-06-24 22:30:50 +0600

    Added dictionary attack

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import requests
 
+file_type_dict = {
+    '1': 'dir_', 
+    '2': 'file_', 
+    '3': 'file_php_', 
+    '4': '(dir-file) '
+    }
+
+word_type_dict = {
+    '1': 'all',
+    '2': 'common',
+    '3': 'crazy',
+    '4': 'extra'
+    }
+
 def get_ans(qs):
     while True:
         ans = input(qs)
@@ -8,9 +22,87 @@ def get_ans(qs):
         elif ans.lower() == 'n':
             return False
 
+def request(url):
+    try:
+        r = requests.get(url)
+        if r.status_code == 404:
+            return False
+        else:
+            return r.status_code
+    except Exception as e:
+        print('\n[!] - OOps. Got error.\n')
+        print(e, '\n')
+        return None
+
 if __name__ == '__main__':
     print('')
     print('\t++++++++++++++++++++++++++++++++++++')
     print('\t|||  Web Directories Discoverer  |||')
     print('\t|||        By @codernayeem       |||')
     print('\t====================================')
+
+    while True:
+        count = 0
+        save = ''
+
+        print('\n[+] - Enter Target link')
+        target = input('[?] --> ')
+
+        if target == '':
+            continue
+
+        if not (target.startswith('http://') or target.startswith('https://')):
+            target = 'http://' + target
+
+        if not target.endswith('/'):
+            target += '/'
+        
+        file_name = ''
+        if get_ans('\n---> Wanna use buitin dictionaries? '):
+            while True:
+                print('\n--> Choose dictionary type? ')
+                print('  1. Only Directory')
+                print('  2. Only Files')
+                print('  3. Only Files (PHP)')
+                print('  4. Directory & Files')
+                ans = input('[>] ')
+                if ans in ['1', '2', '3', '4']:
+                    file_type = ans
+                    break
+            while True:
+                print('\n--> Choose words type? ')
+                print('  1. All')
+                print('  2. Common')
+                print('  3. Crazy')
+                print('  4. Extra')
+                ans = input('[>] ')
+                if ans in ['1', '2', '3', '4']:
+                    word_type = ans
+                    break
+            file_name = file_type_dict[file_type] + word_type_dict[word_type] + '.wordlist'
+            
+        try:
+            fl = open(file_name, 'r')
+        except:
+            print(f'[+] - Failed to open "{file_name}". Make sure that file is in the same directory.\n')
+            continue
+        directory_list = fl.read().strip().splitlines()
+        fl.close()
+
+        for a_item in directory_list:
+            a_item = a_item.strip()
+            if a_item != '':
+                link = target + a_item
+                response = request(link)
+                if response is None:
+                    print('[+] - Got error. So we sttoped.')
+                    break
+                else:
+                    print(f'\n[+] - Got at - {link} - [{response}]')
+                    count += 1
+                    
+        print(f'\n[!] - All done ({count})')
+
+        if not get_ans('\n[?] - Wanna try again? '):
+            exit(0)
+            
